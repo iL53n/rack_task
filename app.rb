02526f6ce736b1340a3d7114408e92fa7214f785
iL53n <ilsegribko@gmail.com>
2019-05-16 13:58:51 +0300

    correct_2

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require_relative 'time_form'
+require_relative 'time_format'
 
 class App
   def call(env)
@@ -6,24 +6,27 @@ class App
     @response = Rack::Response.new
 
     if @request.path_info == "/time"
-      response = TimeFormat.new(@request.params)
-
-      if response.valid?
-        fix_response(200, response.call)
-      else
-        fix_response(400, "Unknown time format #{response.unknown_formats}\n")
-      end
-
+      time_response
     else
-      fix_response(404, "Not found!\n")
+      make_response(404, "Not found!\n")
     end
   end
 
   private
 
-  def fix_response(status, body)
+  def make_response(status, body)
     @response.status = status
     @response.header['ContentType'] = 'text/plain'
     @response.write(body.to_s)
+  end
+
+  def time_response
+    response = TimeFormat.new(@request.params)
+
+    if response.valid?
+      make_response(200, response.call)
+    else
+      make_response(400, "Unknown time format #{response.unknown_formats}\n")
+    end
   end
 end
